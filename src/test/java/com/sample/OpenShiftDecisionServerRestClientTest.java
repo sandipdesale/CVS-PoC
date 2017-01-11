f36fd3e3b4a8dfdefbc8c0a915662e7f7fb8268b
Tejaswi Verma Alluri <tejaswiverma.alluri@cognizant.com>
2017-01-11 13:32:18 -0500

    Working stateful session service calls.

--- src/test/java/com/sample/OpenShiftDecisionServerRestClientTest.java
+++ src/test/java/com/sample/OpenShiftDecisionServerRestClientTest.java
@@ -1,15 +1,12 @@
 package com.sample;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class OpenShiftDecisionServerRestClientTest {
 
-	
 	@Test
 	public void test_new() {
-		OpenShiftDecisionServerRestClient.invokeRulesService1();
+		OpenShiftDecisionServerRestClient.invokeRulesService();
 	}
 
 }
